8c91534c021976a7655987ae8c0828ccdd7f2cc5
John Demos <john.demos@colorado.edu>
2023-03-31 16:45:48 -0600

    Removes redundant non-route methods and sets config to generic example.

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ load_dotenv(verbose=True)
 
 modern_treasury = ModernTreasury(
     # defaults to os.environ.get("MODERN_TREASURY_API_KEY")
-    api_key=os.environ.get("MT_API_KEY"),
-    organization_id=os.environ.get("MT_ORG_ID"),
+    api_key=os.environ.get("KKMT_API_KEY"),
+    organization_id=os.environ.get("KKMT_ORG_ID"),
 )
 
 app = Flask(__name__)
@@ -47,7 +47,7 @@ def render_dashboard():
 @app.route('/payments', methods= ['GET'])
 def list_payments():
 
-    #expected_payments = modern_treasury.expected_payments.list(type='wire', counterparty_id='036118f0-0e41-42e7-9f38-d14031ef1970', created_at_lower_bound= '2023-02-27')
+    # expected_payments = modern_treasury.expected_payments.list(type='wire', created_at_lower_bound= '2023-03-24')
     expected_payments = modern_treasury.expected_payments.list()
     payment_count = str(len(list(enumerate(expected_payments))))
     
@@ -56,7 +56,7 @@ def list_payments():
 @app.route('/distributions')
 def list_distributions():
 
-    #payment_orders = modern_treasury.payment_orders.list(counterparty_id = '036118f0-0e41-42e7-9f38-d14031ef1970', effective_date_end='2023-03-01')
+    # payment_orders = modern_treasury.payment_orders.list(type='wire', status='needs_approval')
     payment_orders = modern_treasury.payment_orders.list()
     payment_count = str(len(list(enumerate(payment_orders))))
     
@@ -64,77 +64,6 @@ def list_distributions():
 
 
 # NON-ROUTE METHODS BELOW
-
-
-# def create_payment_order(payload, org_id, api_key):
-#     url = 'https://app.moderntreasury.com/api/payment_orders'
-#     try:
-#         resp = requests.post(url=url, auth=(org_id, api_key), json=payload)
-#         return resp
-#     except Exception as e:
-#         print(e)
-#         return "Call failed."
-
-
-# def async_create_po(payload, org_id, api_key):
-#     url = "https://app.moderntreasury.com/api/payment_orders/create_async"
-#     try:
-#         response = requests.post(url=url, auth=(org_id, api_key), json=payload)
-#         return response
-#     except Exception as e:
-#         print(e)
-#         return "Call failed."
-
-
-# def create_po(payload, org_id, api_key):
-#     url = "https://app.moderntreasury.com/api/payment_orders"
-#     try:
-#         response = requests.post(url=url, auth=(org_id, api_key), json=payload)
-#         return response
-#     except Exception as e:
-#         print(e)
-#         return "Call failed."
-
-
-# def create_ep_from_form(payload, org_id, api_key):
-#     url = 'https://app.moderntreasury.com/api/expected_payments'
-#     try:
-#         resp = requests.post(url=url, auth=(org_id, api_key), json=payload)
-#         return resp
-#     except Exception as e:
-#         print(e)
-#         return "Call failed."
-
-
-# def create_va(payload, org_id, api_key):
-#     url = "https://app.moderntreasury.com/api/virtual_accounts"
-#     try:
-#         resp = requests.post(url=url, auth=(org_id, api_key), json=payload)
-#         return resp
-#     except Exception as e:
-#         print(e)
-#         return "Call failed."
-
-
-# def list_expected_payments(org_id, api_key, params=None):
-#     url = 'https://app.moderntreasury.com/api/expected_payments'
-#     try:
-#         resp = requests.get(url, auth=(org_id, api_key), params=params)
-#         return resp
-#     except Exception as e:
-#         print(e)
-#         return "Call failed."
-
-
-# def list_payment_orders(org_id, api_key, params=None):
-#     url = 'https://app.moderntreasury.com/api/payment_orders'
-#     try:
-#         resp = requests.get(url, auth=(org_id, api_key), params=params)
-#         return resp
-#     except Exception as e:
-#         print(e)
-#         return "Call failed."
-
 
 def dollars_to_cents(dollars):
     cents = float(dollars) * 100
